c0f6e14b63afcdf6923d3ab7937727ef13131ac6
Yichao <yq2550@gmail.com>
2022-09-04 16:11:24 +0100

    03 exercise int test done

--- 03_unit_testing_peer_classes_bite/lib/secret_diary.rb
+++ 03_unit_testing_peer_classes_bite/lib/secret_diary.rb
@@ -1,21 +1,26 @@
 class SecretDiary
   def initialize(diary) # diary is an instance of Diary
-    # ...
+    @lock = true
+    @diary = diary
   end
 
   def read
     # Raises the error "Go away!" if the diary is locked
+    fail "Go away!" if @lock
     # Returns the diary's contents if the diary is unlocked
+    return @diary.read unless @lock
     # The diary starts off locked
   end
 
   def lock
     # Locks the diary
     # Returns nothing
+    @lock = true
   end
 
   def unlock
     # Unlocks the diary
     # Returns nothing
+    @lock = false
   end
 end
--- 03_unit_testing_peer_classes_bite/spec/ex_int_spec.rb
+++ 03_unit_testing_peer_classes_bite/spec/ex_int_spec.rb
@@ -2,7 +2,24 @@ require "secret_diary"
 require "diary"
 
 RSpec.describe "int test" do
-  xit "locks the diary" do
-    diary = Diary.new
-  end 
+  it "starts off locked and raise error when being called read" do
+    diary = Diary.new("Today is a nice day")
+    secret_diary = SecretDiary.new(diary)
+    expect {secret_diary.read}.to raise_error "Go away!"
+  end
+
+  it "unlocks the diary and returns the content" do
+    diary = Diary.new("Today is a nice day")
+    secret_diary = SecretDiary.new(diary)
+    secret_diary.unlock
+    expect(secret_diary.read).to eq "Today is a nice day"
+  end
+
+  it "locks the diary and raises error" do
+    diary = Diary.new("Today is a nice day")
+    secret_diary = SecretDiary.new(diary)
+    secret_diary.unlock
+    secret_diary.lock
+    expect {secret_diary.read}.to raise_error "Go away!"
+  end
 end
